0b333c162e6849aae256fdc3ed539c8bf2bdd7e1
zahar <genavalera@yahoo.com>
2019-05-21 16:59:30 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+
+
+class unstruct_data(object, edges):
+    def __init__(self):
+        self.points   = [[],[],[]]
+        self.edgesInElement = edges
+        self.elements = [[],[],[]]
+        self.fields = []
+
+    def read_tec(self, filename):
+        pass
+
+    def read_stl(self, filename):
+        pass
+
